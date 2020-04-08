ace7f4a2ecb08c831179638c6964056b5559ac58
KKiixmiix <61215406+KKiixmiix@users.noreply.github.com>
2020-04-07 23:35:13 -0400

    Trigger and extras
    Mushrooms added at three locations
    Red Runestone added to Spire mountain
    Lanterns added to Mountain Path
    crystalKey heartstone trigger
    
    Deleted ship

---
+++ Escape to Space Mountain/Assets/NatureAssets/Magical Rock/Scripts/Example/SwitchStateOnClick.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+namespace OldTreeStudio.Example
+{
+	public class SwitchStateOnClick : MonoBehaviour
+	{
+		public GameObject _magicalRockGO;
+		public GameObject _player;
+		private bool on = false;
+		private readonly float limit = 3f;
+
+		private IMRock _mRock;			//It's better to use rock simplified interface
+
+
+		private void Start ()
+		{
+			//Accessing the interface of MRock
+			_mRock = _magicalRockGO.GetComponent<IMRock> ();
+			_mRock.SwitchState();
+		}
+
+		private void Update ()
+		{
+			//Simple switch state on a screen click event
+			float distance = Vector3.Distance(_magicalRockGO.transform.position, _player.transform.position);
+			if (distance < limit && !on)
+            {
+				_mRock.SwitchState();
+				on = true;
+			}
+				
+
+		}
+	}
+}
