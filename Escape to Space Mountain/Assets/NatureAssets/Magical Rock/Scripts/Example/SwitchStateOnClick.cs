0ed705f161ea4e49263c54fa331ee8953179d787
KKiixmiix <61215406+KKiixmiix@users.noreply.github.com>
2020-04-23 17:39:53 -0400

    Halo
    Halo and stone trigger script added.

--- Escape to Space Mountain/Assets/NatureAssets/Magical Rock/Scripts/Example/SwitchStateOnClick.cs
+++ Escape to Space Mountain/Assets/NatureAssets/Magical Rock/Scripts/Example/SwitchStateOnClick.cs
@@ -3,34 +3,53 @@
 
 namespace OldTreeStudio.Example
 {
-	public class SwitchStateOnClick : MonoBehaviour
-	{
-		public GameObject _magicalRockGO;
-		public GameObject _player;
-		private bool on = false;
-		private readonly float limit = 3f;
+    public class SwitchStateOnClick : MonoBehaviour
+    {
+        public GameObject _pickedUpGO;
+        public GameObject _magicalRockGO;
+        public GameObject _player;
+        public Behaviour playHalo;
+        public Behaviour pugoHalo;
+        private bool on = false;
+        private bool up = false;
+        public readonly float limit = 3f;
 
-		private IMRock _mRock;			//It's better to use rock simplified interface
+        private IMRock _mRock;          //It's better to use rock simplified interface
 
 
-		private void Start ()
-		{
-			//Accessing the interface of MRock
-			_mRock = _magicalRockGO.GetComponent<IMRock> ();
-			_mRock.SwitchState();
-		}
+        private void Start()
+        {
+            //Accessing the interface of MRock
+            _mRock = _magicalRockGO.GetComponent<IMRock>();
+            _mRock.SwitchState();
+            pugoHalo = (Behaviour)_pickedUpGO.GetComponent("Halo");
+            pugoHalo.enabled = true;
+            playHalo = (Behaviour)_player.GetComponent("Halo");
+            playHalo.enabled = false;
+        }
 
-		private void Update ()
-		{
-			//Simple switch state on a screen click event
-			float distance = Vector3.Distance(_magicalRockGO.transform.position, _player.transform.position);
-			if (distance < limit && !on)
-            {
-				_mRock.SwitchState();
-				on = true;
-			}
-				
-
-		}
-	}
+        private void Update()
+        {
+            Vector3 player = _player.transform.position;
+            Vector3 picked = _pickedUpGO.transform.position;
+            //Debug.Log("Player:" + player + ", PUOb r: " + picked + "; distance: " + Vector3.Distance(player, picked));
+            if (!up && limit > Vector3.Distance(player, _pickedUpGO.transform.position)) {
+                Debug.Log("Player position (up): " + player + ", " + picked);
+                up = true;
+                pugoHalo.enabled = false;
+                playHalo.enabled = true;
+            }
+            if (up && !on) {
+                Vector3 rock = _magicalRockGO.transform.position;
+                //Debug.Log("Player position (up && !on): " + player + ". Rock: " + rock + ". Distance: " + Vector3.Distance(player, rock));
+                if (limit > Vector3.Distance(player, _magicalRockGO.transform.position)) {
+                    Debug.Log("Player position (up && !on): " + player + ". Rock: " + rock + ". Distance: " + Vector3.Distance(player, rock));
+                    //Debug.Log("Player position (limit > MR.pos): " + player);
+                    _mRock.SwitchState();
+                    on = true;
+                    playHalo.enabled = false;
+                }
+            }
+        }
+    }
 }
