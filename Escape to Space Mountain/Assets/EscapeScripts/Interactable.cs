d22a27b8f56031c4095eeb8ce1029f30ddc904d5
KKiixmiix <61215406+KKiixmiix@users.noreply.github.com>
2020-05-04 17:22:24 -0400

    Back to First Person

---
+++ Escape to Space Mountain/Assets/EscapeScripts/Interactable.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+
+    bool isFocus = false;
+    protected Transform player;
+
+    bool hasInteracted = false;
+
+    // Override this method in inheriting classes.
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+    private void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+            if (distance < radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
